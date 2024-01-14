38d4a8b7c713e40683e810d747a99b7971f5eafe
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2024-01-14 17:41:55 +0600

    vertical

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,13 @@
 interface ButtonProps {
   title: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
-const Button: React.FC<ButtonProps> = ({ title }) => {
-  return <button className="p-4 border mx-2">{title}</button>;
+const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
+  return (
+    <button className="p-4 border mx-2" onClick={onClick}>
+      {title}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import Button from "./button";
 
 function Canvas() {
+  const [splitter, setSplitter] = useState(false);
+  const verticalSplitting = () => {
+    setSplitter(true);
+  };
+  const horizontalSplitting = () => {
+    console.log("h");
+  };
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <Button title="V" />
-      <Button title="H" />
+    <div
+      className="h-screen
+      flex items-center border justify-center bg-red-400"
+    >
+      <Button title="V" onClick={verticalSplitting} />
+      <Button title="H" onClick={horizontalSplitting} />
+      <div>{splitter && <Canvas />}</div>
     </div>
   );
 }
